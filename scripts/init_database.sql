43069748fae692054629d4c562f658a9436b16d4
SougataJana <73524162+SougataJana@users.noreply.github.com>
2025-03-01 22:38:45 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,19 @@
+use master;
+GO
+
+if exists (select 1 from  sys.databases where name='Datawarehouse')
+BEGIN
+   alter database Datawarehouse set single_user with rollback immediate;
+   drop database Datawarehouse ;
+   end;
+   go
+create database Datawarehouse;
+use Datawarehouse;
+
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
+GO
 
